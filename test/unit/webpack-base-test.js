94e29c6ffa18892711564b3daaf83bfd27c53610
xx <xx@xxdeMac-mini.local>
2024-09-23 13:35:31 +0800

    fix: 单元测试路径问题

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -1,17 +1,21 @@
 const assert = require("assert");
+const path = require("path");
 
 describe("webpack.base.js test case", () => {
   const baseConfig = require("../../lib/webpack.base.js");
 
   it("entry", () => {
-    assert.equal(
-      baseConfig.entry.index,
-      "/Users/xx/Desktop/myproject/webpack-base-builder/test/smoke/template/src/index/index.js"
+    // 使用动态路径构建期望的路径
+    const expectedIndexPath = path.join(
+      __dirname,
+      "../smoke/template/src/index/index.js"
+    );
+    const expectedSearchPath = path.join(
+      __dirname,
+      "../smoke/template/src/search/index.js"
     );
 
-    assert.equal(
-      baseConfig.entry.search,
-      "/Users/xx/Desktop/myproject/webpack-base-builder/test/smoke/template/src/search/index.js"
-    );
+    assert.equal(baseConfig.entry.index, expectedIndexPath);
+    assert.equal(baseConfig.entry.search, expectedSearchPath);
   });
 });
